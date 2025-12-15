541c7a1ea87f15d3b79314c60bdc9ebb63c2250a
daniel <danielhoon@naver.com>
2025-12-15 20:40:37 +0900

    프로젝트2 레프로핏클라이언트 설정변경

--- app/src/main/java/com/example/a20251215/RetrofitClient.kt
+++ app/src/main/java/com/example/a20251215/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "https://www.maribot.monster/"
+    private const val BASE_URL = "https://www.maribot.monster/Project2/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
