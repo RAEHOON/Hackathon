5991b5a037321fe060be8cb1762762c2161a572c
daniel <danielhoon@naver.com>
2025-12-16 01:41:03 +0900

    서버관련 모두 완료

--- app/src/main/java/com/example/a20251215/Ranking/RankingFragment.kt
+++ app/src/main/java/com/example/a20251215/Ranking/RankingFragment.kt
@@ -1,10 +1,11 @@
-package com.example.a20251215
+package com.example.a20251215.Ranking
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.a20251215.R
 
 class RankingFragment  : Fragment() {
 
