646c64a080f84f60389d5bcb43e6fcfe9b9b197d
Eron <khaps6404@naver.com>
2025-12-15 21:29:44 +0900

    eron 21:29

--- app/src/main/java/com/example/a20251215/Sign/SignupPasswordFragment.kt
+++ app/src/main/java/com/example/a20251215/Sign/SignupPasswordFragment.kt
@@ -5,28 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.a20251215.R
 
-class SignupEmailFragment : Fragment() {
+class SignupPasswordFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_signup_email, container, false)
+        return inflater.inflate(R.layout.fragment_signup_password, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val confirmBtn = view.findViewById<Button>(R.id.btn_find_id_request)
+        val finishbtn = view.findViewById<Button>(R.id.btn_signup_finish)
 
-        confirmBtn.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, SignupNicknameFragment())
-                .addToBackStack(null)
-                .commit()
+        finishbtn.setOnClickListener {
+            Toast.makeText(context, "회원가입 완료", Toast.LENGTH_SHORT).show()
+
+            activity?.finish()
         }
     }
 }
--- app/src/main/java/com/example/a20251215/SignUpActivity.kt
+++ app/src/main/java/com/example/a20251215/SignUpActivity.kt
@@ -1,10 +1,7 @@
 package com.example.a20251215
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.a20251215.Sign.SignupEmailFragment
 
 class SignUpActivity : AppCompatActivity() {
