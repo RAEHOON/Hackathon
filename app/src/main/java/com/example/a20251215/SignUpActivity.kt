e68792e044129430921c3e6af8557da62bac418c
Eron <khaps6404@naver.com>
2025-12-15 20:33:02 +0900

    eron 20:31

--- app/src/main/java/com/example/a20251215/SignUpActivity.kt
+++ app/src/main/java/com/example/a20251215/SignUpActivity.kt
@@ -5,16 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.a20251215.Sign.SignupEmailFragment
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, SignupEmailFragment())
+                .commit()
         }
     }
 }
