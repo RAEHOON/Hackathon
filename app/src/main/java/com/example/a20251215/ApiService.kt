15cda0c9ae4fb3cf622a28a3792a0c6a24112208
daniel <danielhoon@naver.com>
2025-12-15 18:15:24 +0900

    apiservice 작성 회원가입 부분

--- app/src/main/java/com/example/a20251215/ApiService.kt
+++ app/src/main/java/com/example/a20251215/ApiService.kt
@@ -2,10 +2,19 @@ package com.example.a20251215
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface ApiService {
 
+    @FormUrlEncoded
     @POST("signup.php")
-    fun signup(@Body request: SignupRequest): Call<SignupResponse>
+    fun signup(
+        @Field("username") username: String,
+        @Field("nickname") nickname: String,
+        @Field("loginid") loginid: String,
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<SignupResponse>
 }
--- app/src/main/java/com/example/a20251215/SignupRequest.kt
+++ app/src/main/java/com/example/a20251215/SignupRequest.kt
@@ -3,9 +3,9 @@ package com.example.a20251215
 import com.google.gson.annotations.SerializedName
 
 data class SignupRequest(
-    @SerializedName("name") val name: String,
+    @SerializedName("username") val username: String,
     @SerializedName("nickname") val nickname: String,
-    @SerializedName("userid") val userid: String,
+    @SerializedName("loginid") val loginid: String,
     @SerializedName("email") val email: String,
     @SerializedName("password") val password: String
 )
