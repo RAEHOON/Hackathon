892f83e1751eeb8c38116d6497b50ca077d32648
shin1231231 <2018rk@naver.com>
2025-12-16 09:01:19 +0900

    commit 이삭 - 랭킹 수정중2

--- app/src/main/java/com/example/a20251215/Ranking/RankModels.kt
+++ app/src/main/java/com/example/a20251215/Ranking/RankModels.kt
@@ -1,16 +1,19 @@
 package com.example.a20251215.Ranking
 
- enum class RankType { BEST, WORST }
+// BEST / WORST 구분
+enum class RankType { BEST, WORST }
 
- data class UserScore(
+// 서버에서 받아온 랭킹을 계산용으로 변환한 모델
+data class UserScore(
     val userId: Int,
     val name: String,
     val certCount: Int
 )
 
+// RecyclerView 한 줄(UI) 모델
 data class RankItem(
-    val badge: String,
+    val badge: String,  // 🥇 / 🥈 / 🥉 / 😡 / 😭 / 😮‍💨 / "4"
     val name: String,
-    val count: String,
-    val sub: String
+    val count: String,  // "인증 10회"
+    val sub: String     // "이번 달 1등" / "이번 달 워스트 1"
 )
